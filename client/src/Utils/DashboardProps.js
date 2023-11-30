f2c6abf9b89fd05669f68ac80fe349ae4d7a0fb5
Shiwhy <142288699+Shiwhy@users.noreply.github.com>
2023-11-30 18:02:31 +0530

    Add files via upload

--- client/src/Utils/DashboardProps.js
+++ client/src/Utils/DashboardProps.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Shery from 'sheryjs';
+
 
 export default function DashboardProps(props) {
+
+  
+  Shery.makeMagnet(".card", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+
   return (
     <>
       <div className="col">
-        <div className="card">
-          <div className="dashboard-icon">
-            <p>{props.icon}</p>
+        <NavLink to={props.path}>
+          <div className="card">
+            <div className="dashboard-icon">
+              <p>{props.icon}</p>
+            </div>
+            <div className="card-title">
+              <p>{props.title}</p>
+            </div>
+            <div className="card-value">
+              <p>{props.value}</p>
+            </div>
           </div>
-          <div className="card-title">
-            <p>{props.title}</p>
-          </div>
-          <div className="card-value">
-            <p>{props.value}</p>
-          </div>
-        </div>
+        </NavLink>
     </div>
     </>
   )
