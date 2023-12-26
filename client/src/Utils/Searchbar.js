8d8c3d17fc215268fc0d1bee6c604accc21a4b1b
Shiwhy <142288699+Shiwhy@users.noreply.github.com>
2023-12-26 17:55:50 +0530

    Add files via upload

---
+++ client/src/Utils/Searchbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../css/search.css';
+
+import { IoSearch } from "react-icons/io5";
+
+
+const Searchbar = (props) => {
+
+  return (
+    <>
+    <div className="search">
+      <input type="text" 
+        placeholder={ props.placeholder } 
+        value={props.value}
+        name={props.name} 
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+      <button onClick={ props.onClick }> <IoSearch /></button>
+    </div>      
+    </>
+  )
+}
+
+export default Searchbar
